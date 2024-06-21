32f39d909baf3cb29ff8336217a31acebfd3b6ec
Richard Miao <richardmiao1111@gmail.com>
2024-06-20 21:14:44 -0400

    changed ddl queries as per relational diagram req

--- backend/db_queries/ddl_queries.py
+++ backend/db_queries/ddl_queries.py
@@ -1,28 +1,28 @@
 # DATA DEFINITION LANGUAGE QUERIES
 
-createRecipeTable ="""
+createRecipeTable = """
     CREATE TABLE Recipes (
         id INT NOT NULL,
         recipe_name VARCHAR(100) NOT NULL,
-        description VARCHAR(200),
-        ingredients TEXT [] NOT NULL,
-        serving_size INT NOT NULL,
-        servings INT NOT NULL,
-        steps VARCHAR(800) NOT NULL,
-        PRIMARY KEY (id)
+        rdescription VARCHAR(200),
+        ringredients TEXT [] NOT NULL,
+        rserving_size INT NOT NULL,
+        rservings INT NOT NULL,
+        rsteps VARCHAR(800) NOT NULL,
+        PRIMARY KEY (rId)
     );
 """
 createSearchTermsTable = """
-    CREATE TABLE Search_Terms (
-        recipeId INT NOT NULL,
+    CREATE TABLE RecipeSearchTerms (
+        rId INT NOT NULL,
         terms VARCHAR(20) NOT NULL,
-        PRIMARY KEY (recipeId, terms),
-        FOREIGN KEY (recipeId) REFERENCES Recipes(id)
+        PRIMARY KEY (rId, terms),
+        FOREIGN KEY (rId) REFERENCES Recipes(id)
     );
 """
 createIngredientTable = """
-        CREATE TABLE Nutrition (
-        nutrition_name VARCHAR(50) NOT NULL,
+        CREATE TABLE Ingredients (
+        iname VARCHAR(50) NOT NULL,
         protein FLOAT,
         carb FLOAT,
         fat FLOAT,
@@ -30,16 +30,38 @@ createIngredientTable = """
         sodium FLOAT,
         vitamin_d FLOAT,
         vitamin_e FLOAT,
-        PRIMARY KEY (nutrition_name)
+        PRIMARY KEY (iname)
     );
 """
 createIngredientInRecipeTable = """
-    CREATE TABLE Food_Prices (
-        food_name VARCHAR(30) NOT NULL,
-        food_quantity INT NOT NULL,
-        food_metric INT NOT NULL,
+    CREATE TABLE Prices (
+        pname VARCHAR(30) NOT NULL,
+        pquantity INT NOT NULL,
+        pmetric INT NOT NULL,
         price FLOAT NOT NULL,
         PRIMARY KEY (food_name)
+    );
+"""
+
+createIngredientCostsRelation = """
+    CREATE TABLE IngredientCosts (
+        iname VARCHAR(50) NOT NULL,
+        pname FLOAT NOT NULL,
+        PRIMARY KEY (iname, pname),
+        FOREIGN KEY (iname) REFERENCES Ingredients(iname),
+        FOREIGN KEY (pname) REFERENCES Prices(pname)
+    );
+"""
+
+createIngredientsInRecipeRelation = """
+    CREATE TABLE IngredientsInRecipe (
+        rId INT NOT NULL,
+        iname VARCHAR(50) NOT NULL,
+        unit_of_measure VARCHAR(15) NOT NULL,
+        quantity FLOAT NOT NULL,
+        PRIMARY KEY (recipeId, iname),
+        FOREIGN KEY (recipeId) REFERENCES Recipes(id),
+        FOREIGN KEY (iname) REFERENCES Ingredients(iname)
     );
 """
 
