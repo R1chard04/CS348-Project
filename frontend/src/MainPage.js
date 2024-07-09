fa1f5ea5f39dc2fad245a257c0939ebb1e0f1b09
Thomas Wang <tswang@uwaterloo.ca>
2024-07-09 03:42:09 -0400

    add placeholder search bars

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -13,6 +13,21 @@ const basicSearchValToEndpoint = {
     recipe_name: 'getrecipebyname',
 };
 
+const priceSearchValToEndpoint = {
+    id: 'getpricebyid',
+    recipe_name: 'getrecipebyname',
+};
+
+const mostExpensiveSearchValToEndpoint = {
+    id: 'getpricebyid',
+    recipe_name: 'getrecipebyname',
+};
+
+const mostProteinSearchValToEndpoint = {
+    id: 'getpricebyid',
+    recipe_name: 'getrecipebyname',
+};
+
 const MainPage = () => {
     const [message, setMessage] = useState('');
     const [recipe, setRecipe] = useState([]);
@@ -53,14 +68,23 @@ const MainPage = () => {
                 <option value="Basic Search">Basic Search</option>
                 <option value="Advanced Search">Advanced Search</option>
                 <option value="Nutrition Search">Nutrition Search</option>
+                <option value="Price Search">Price Search</option>
+                <option value="Most Expensive Search">Most Expensive Search</option>
+                <option value="Most Protein Search">Most Protein Search</option>
             </select>
             {searchOption === 'Basic Search' && <SearchBar setRecipe={setRecipe} valToEndpoint={basicSearchValToEndpoint} />}
             {searchOption === 'Advanced Search' && <ListBar itemList={ingredients} setRecipe={setRecipe} />}
             {searchOption === 'Nutrition Search' && <SearchBar setRecipe={setRecipe} valToEndpoint={nutritionSearchValToEndpoint} />}
+            {searchOption === 'Price Search' && <SearchBar setRecipe={setRecipe} valToEndpoint={priceSearchValToEndpoint} />}
+            {searchOption === 'Most Expensive Search' && <ListBar setRecipe={setRecipe} itemList={ingredients}/>}
+            {searchOption === 'Most Protein Search' && <ListBar setRecipe={setRecipe} itemList={ingredients}/>}
 
             {recipe.length > 0 && searchOption === 'Basic Search' && <Table rows={recipe} tableType="recipe" />}
             {recipe.length > 0 && searchOption === 'Advanced Search' && <Table rows={recipe} tableType="recipe" />}
             {recipe.length > 0 && searchOption === 'Nutrition Search' && <Table rows={recipe} tableType="nutrition" />}
+            {recipe.length > 0 && searchOption === 'Price Search' && <Table rows={recipe} tableType="price" />}
+            {recipe.length > 0 && searchOption === 'Most Expensive Search' && <Table rows={recipe} tableType="price" />}
+            {recipe.length > 0 && searchOption === 'Most Protein Search' && <Table rows={recipe} tableType="nutrition" />}
         </div>
     );
 };
