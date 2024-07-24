1e06f1abed07cd17be2f8fdffb64c16b04648fe0
RYZEXY <sunfred88@gmail.com>
2024-07-24 18:24:28 -0400

    new fancy feature, not connected w ends

---
+++ backend/fancy_features/recommended_recipe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from db_queries.feature_queries import get_low_cal_recipe
+from db_queries.feature_queries import get_high_cal_recipe
+
+def recommend_recipe(bmi):
+    if bmi >= 25:
+        query = get_low_cal_recipe()
+    else:
+        query = get_high_cal_recipe()
+    return query
